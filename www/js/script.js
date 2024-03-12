88019469473b8590006cbbd1668ee3f2a3bb0f56
Robert-luiz <roluiz789@gmail.com>
2024-03-12 23:40:18 +0900

    Adição de sistema basico de login

---
+++ www/js/script.js
@@ -0,0 +1,30 @@
+/*
+function Login(){
+
+var nomeV = "Vitor"
+var senhaV = "123"
+
+var nome = document.getElementById("Cnome").value
+var senha = document.getElementById("Csenha").value
+
+if (nome == nomeV && senha == senhaV){
+
+alert("Login feito")
+
+window.location.href = "shrek.html"
+
+}else{
+
+alert("Senha ou usuario errados")
+
+}
+
+}
+*/
+
+$(document).ready(function(){
+  $("button").click(function(){
+    $("a").hide();
+  });
+});
+
